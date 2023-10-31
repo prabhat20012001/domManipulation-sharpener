6340f4defa84eb3fba6a7441e436d6c39e9797ad
prabhat20012001 <prabhatkumarmnnit@gmail.com>
2023-10-31 22:44:53 +0530

    dom part-1

---
+++ dom.js
@@ -0,0 +1,36 @@
+// console.dir(document)
+
+
+// Examine the document object //
+
+// console.log(document.domain)
+// console.log(document.URL)
+// console.log(document.title)
+// document.title=123
+// console.log(document.doctype)
+// console.log(document.head)
+// console.log(document.body)
+// document.body
+// document.head
+// console.log(document.all)
+// document.all[10].textContent="Prabhat"
+// console.log(document.forms)
+// console.log(document.links)
+// console.log(document.images)
+
+// ==========Get element By Id==============================
+
+// let hed=document.getElementById("header-title")
+// let header=document.getElementById("main-header")
+// hed.textContent="Hello"
+// hed.innerText="GoodBye"
+// hed.innerHTML="<h3>Helloooo</h3>"
+// header.style.borderBottom="solid 3px black"
+
+
+// ==============Get Element By Class Name =================//
+// var items=document.getElementsByClassName("list-group-item")[0].innerText="hey"
+// console.log(items[0])
+
+let title=document.getElementsByClassName("title")[0]
+title.style.color="green"
