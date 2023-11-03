799117b2744c5896978e6721dde449ab58a6abf3
prabhat20012001 <prabhatkumarmnnit@gmail.com>
2023-11-03 15:00:19 +0530

    delete button added

--- form.js
+++ form.js
@@ -1,42 +1,63 @@
-const myForm=document.querySelector('#my-form')
-const nameInput=document.querySelector('#name')
-const emailInput=document.querySelector('#email')
-const msj=document.querySelector(".msj")
-const userList=document.querySelector("#users")
+  const myForm = document.querySelector('#my-form');
+        const nameInput = document.querySelector('#name');
+        const emailInput = document.querySelector('#email');
+        const msj = document.querySelector('.msj');
+        const userList = document.querySelector('#users');
 
+        myForm.addEventListener('submit', onSubmit);
 
-myForm.addEventListener('submit',onSubmit)
+        // Retrieve data from local storage
+        let userData = JSON.parse(localStorage.getItem('userData')) || [];
 
-function onSubmit(e){
-    e.preventDefault();
-if(nameInput.value==''|| emailInput.value==''){
-    msj.classList.add('error');
-msj.innerHTML="please enter all fields"
-setTimeout(()=>msj.remove(),1000)
-}else{
-const li=document.createElement('li')
-li.appendChild(document.createTextNode(`${nameInput.value}:${emailInput.value}`))
+        // Render the existing data when the page loads
+        renderUserList();
 
-const userDataJSON = localStorage.getItem('userData');
+        function onSubmit(e) {
+            e.preventDefault();
+            if (nameInput.value == '' || emailInput.value == '') {
+                msj.classList.add('error');
+                msj.innerHTML = 'Please enter all fields';
+                setTimeout(() => msj.remove(), 1000);
+            } else {
+                const userDataItem = {
+                    name: nameInput.value,
+                    email: emailInput.value,
+                };
 
-// Parse the JSON string back into a JavaScript object
-const userData = JSON.parse(userDataJSON) || {}; // Initialize an empty object if there's no existing data
+                // Add the new data to the array
+                userData.push(userDataItem);
 
-// Create a new user object
-const newUser = {
-  name: nameInput.value,
-  email: emailInput.value
-};
+                // Store the updated object as a JSON string in local storage
+                localStorage.setItem('userData', JSON.stringify(userData));
 
-// Append the new user data to the existing object
-const newUserId = Object.keys(userData).length + 1;
-userData[newUserId] = newUser;
+                nameInput.value = '';
+                emailInput.value = '';
 
-// Store the updated object as a JSON string in local storage
-localStorage.setItem('userData', JSON.stringify(userData));
+                // Render the updated user list
+                renderUserList();
+            }
+        }
 
-nameInput.value=''
-emailInput.value=''
-}
+        function renderUserList() {
+            // Clear the existing user list
+            userList.innerHTML = '';
 
-}
+            // Render each user item with a delete button
+            userData.forEach((userDataItem, index) => {
+                const li = document.createElement('li');
+                li.appendChild(document.createTextNode(`${userDataItem.name}: ${userDataItem.email}`));
+
+                const deleteButton = document.createElement('button');
+                deleteButton.innerHTML = 'Delete';
+
+                // Add an event listener to delete the data when the button is clicked
+                deleteButton.addEventListener('click', () => {
+                    userData.splice(index, 1);
+                    localStorage.setItem('userData', JSON.stringify(userData));
+                    renderUserList();
+                });
+
+                li.appendChild(deleteButton);
+                userList.appendChild(li);
+            });
+        }
